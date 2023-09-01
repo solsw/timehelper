efb56cebb233728b5bef73bc78f0011ee5f381ae
Oleg Subachev <git@solsw.ru>
2023-09-01 15:35:07 +0500

    docs updated

--- valid.go
+++ valid.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// ValidMonth checks if 'month' represents a valid month.
+// ValidMonth reports whether 'month' represents a valid month.
 func ValidMonth(month time.Month) error {
 	if !(time.January <= month && month <= time.December) {
 		return fmt.Errorf("invalid month '%d'", month)
@@ -13,7 +13,7 @@ func ValidMonth(month time.Month) error {
 	return nil
 }
 
-// ValidDate checks if 'year', 'month', 'day' constitute a valid date.
+// ValidDate reports whether 'year', 'month', 'day' constitute a valid date.
 func ValidDate(year int, month time.Month, day int) error {
 	if err := ValidMonth(month); err != nil || !(1 <= day && day <= DaysInMonth(year, month)) {
 		return fmt.Errorf("invalid date '%d-%d-%d'", year, month, day)
@@ -21,7 +21,7 @@ func ValidDate(year int, month time.Month, day int) error {
 	return nil
 }
 
-// ValidClockTime checks if 'hour', 'min', 'sec' constitute a valid clock time.
+// ValidClockTime reports whether 'hour', 'min', 'sec' constitute a valid clock time.
 func ValidClockTime(hour, min, sec int) error {
 	if !(0 <= hour && hour <= 23 && 0 <= min && min <= 59 && 0 <= sec && sec <= 59) {
 		return fmt.Errorf("invalid clock time '%d:%d:%d'", hour, min, sec)
@@ -29,7 +29,7 @@ func ValidClockTime(hour, min, sec int) error {
 	return nil
 }
 
-// ValidWeekday checks if 'weekday' is valid.
+// ValidWeekday reports whether 'weekday' is valid.
 func ValidWeekday(weekday time.Weekday) error {
 	if !(time.Sunday <= weekday && weekday <= time.Saturday) {
 		return fmt.Errorf("invalid weekday '%d'", weekday)
