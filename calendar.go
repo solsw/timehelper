b777aeb97085e2757e3d7637b59f74ffb90cdfb5
Oleg Subachev <oleg.subachev@groupdocs.com>
2023-09-18 21:25:12 +0500

    GregorianToJulian added

--- calendar.go
+++ calendar.go
@@ -14,29 +14,49 @@ func JulianToGregorian(julian Date) (Date, error) {
 		return Date{}, err
 	}
 	j := julian.Time()
-	if j.Before(TimeYMD(1300, time.March, 1)) || j.After(TimeYMD(2400, time.February, 28)) {
+	if j.Before(TimeYMD(1582, time.October, 5)) || j.After(TimeYMD(2200, time.February, 14)) {
 		return Date{}, fmt.Errorf("unsupported Julian date: %d-%d-%d", julian.Year, julian.Month, julian.Day)
 	}
 	var g time.Time
 	switch {
-	case julian.Time().After(TimeYMD(2300, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 16)
-	case julian.Time().After(TimeYMD(2200, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 15)
-	case julian.Time().After(TimeYMD(2100, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 14)
-	case julian.Time().After(TimeYMD(1900, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 13)
-	case julian.Time().After(TimeYMD(1800, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 12)
-	case julian.Time().After(TimeYMD(1700, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 11)
-	case julian.Time().After(TimeYMD(1500, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 10)
-	case julian.Time().After(TimeYMD(1400, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 9)
-	case julian.Time().After(TimeYMD(1300, time.February, 28)):
-		g = julian.Time().AddDate(0, 0, 8)
+	case j.After(TimeYMD(2100, time.February, 15)):
+		g = j.AddDate(0, 0, 14)
+	case j.After(TimeYMD(1900, time.February, 16)):
+		g = j.AddDate(0, 0, 13)
+	case j.After(TimeYMD(1800, time.February, 17)):
+		g = j.AddDate(0, 0, 12)
+	case j.After(TimeYMD(1700, time.February, 18)):
+		g = j.AddDate(0, 0, 11)
+	default:
+		g = j.AddDate(0, 0, 10)
 	}
 	return Date{g.Year(), g.Month(), g.Day()}, nil
+}
+
+// GregorianToJulian converts [Gregorian] date to [Julian] date.
+//
+// [Gregorian]: https://en.wikipedia.org/wiki/Gregorian_calendar
+// [Julian]: https://en.wikipedia.org/wiki/Julian_calendar
+func GregorianToJulian(gregorian Date) (Date, error) {
+	if err := gregorian.Valid(); err != nil {
+		return Date{}, err
+	}
+	g := gregorian.Time()
+	if g.Before(TimeYMD(1582, time.October, 15)) || g.After(TimeYMD(2200, time.February, 28)) {
+		return Date{}, fmt.Errorf("unsupported Gregorian date: %d-%d-%d", gregorian.Year, gregorian.Month, gregorian.Day)
+	}
+	var j time.Time
+	switch {
+	case g.After(TimeYMD(2100, time.February, 28)):
+		j = g.AddDate(0, 0, -14)
+	case g.After(TimeYMD(1900, time.February, 28)):
+		j = g.AddDate(0, 0, -13)
+	case g.After(TimeYMD(1800, time.February, 28)):
+		j = g.AddDate(0, 0, -12)
+	case g.After(TimeYMD(1700, time.February, 28)):
+		j = g.AddDate(0, 0, -11)
+	default:
+		j = g.AddDate(0, 0, -10)
+	}
+	return Date{j.Year(), j.Month(), j.Day()}, nil
 }
--- calendar_test.go
+++ calendar_test.go
@@ -17,18 +17,18 @@ func TestJulianToGregorian(t *testing.T) {
 		wantErr bool
 	}{
 		{name: "1e",
-			args:    args{julian: Date{1000, time.June, 23}},
+			args:    args{julian: Date{1500, time.June, 23}},
 			want:    Date{},
 			wantErr: true,
 		},
 		{name: "2e",
-			args:    args{julian: Date{3000, time.June, 23}},
+			args:    args{julian: Date{2300, time.June, 23}},
 			want:    Date{},
 			wantErr: true,
 		},
 		{name: "1",
-			args: args{julian: Date{1960, time.June, 23}},
-			want: Date{1960, time.July, 6},
+			args: args{julian: Date{1960, time.June, 10}},
+			want: Date{1960, time.June, 23},
 		},
 		{name: "2",
 			args: args{julian: Date{1821, time.October, 30}},
@@ -52,6 +52,57 @@ func TestJulianToGregorian(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("JulianToGregorian() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGregorianToJulian(t *testing.T) {
+	type args struct {
+		gregorian Date
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Date
+		wantErr bool
+	}{
+		{name: "1e",
+			args:    args{gregorian: Date{1500, time.June, 23}},
+			want:    Date{},
+			wantErr: true,
+		},
+		{name: "2e",
+			args:    args{gregorian: Date{2300, time.June, 23}},
+			want:    Date{},
+			wantErr: true,
+		},
+		{name: "1",
+			args: args{gregorian: Date{1960, time.June, 23}},
+			want: Date{1960, time.June, 10},
+		},
+		{name: "2",
+			args: args{gregorian: Date{1821, time.November, 11}},
+			want: Date{1821, time.October, 30},
+		},
+		{name: "3",
+			args: args{gregorian: Date{1672, time.June, 9}},
+			want: Date{1672, time.May, 30},
+		},
+		{name: "4",
+			args: args{gregorian: Date{1584, time.March, 28}},
+			want: Date{1584, time.March, 18},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GregorianToJulian(tt.args.gregorian)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GregorianToJulian() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GregorianToJulian() = %v, want %v", got, tt.want)
 			}
 		})
 	}
