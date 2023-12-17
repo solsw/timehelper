fcd812ecd1de97387b314580d6bbdf5b881406a1
Oleg Subachev <git@solsw.ru>
2023-12-17 22:21:09 +0500

    UnixTime.MarshalBinary, UnixTime.UnmarshalBinary removed; UnixTime.ParseSecs, UnixTime.ParseMSecs added

--- unixtime.go
+++ unixtime.go
@@ -1,8 +1,7 @@
 package timehelper
 
 import (
-	"bytes"
-	"encoding/binary"
+	"math/bits"
 	"strconv"
 	"time"
 )
@@ -16,40 +15,13 @@ func (t UnixTime) String() string {
 	return strconv.FormatInt(time.Time(t).Unix(), 10)
 }
 
-// MarshalBinary implements the [encoding.BinaryMarshaler] interface.
-// MarshalBinary returns nil error.
-func (t UnixTime) MarshalBinary() ([]byte, error) {
-	bb := make([]byte, binary.MaxVarintLen64)
-	n := binary.PutVarint(bb, time.Time(t).Unix())
-	return bb[:n], nil
-}
-
-// used for testing
-func unixTimeUnmarshalBin(bb []byte) (*UnixTime, error) {
-	sec, err := binary.ReadVarint(bytes.NewReader(bb))
-	if err != nil {
-		return nil, err
-	}
-	ut := UnixTime(time.Unix(sec, 0).In(time.UTC))
-	return &ut, nil
-}
-
-// UnmarshalBinary implements the [encoding.BinaryUnmarshaler] interface.
-func (t *UnixTime) UnmarshalBinary(data []byte) error {
-	ut, err := unixTimeUnmarshalBin(data)
-	if err != nil {
-		return err
-	}
-	*t = *ut
-	return nil
-}
-
 // MarshalText implements the [encoding.TextMarshaler] interface.
 func (t UnixTime) MarshalText() ([]byte, error) {
 	return []byte(t.String()), nil
 }
 
-func unixTimeUnmarshalStr(str string) (*UnixTime, error) {
+// used for testing
+func unmarshalStr(str string) (*UnixTime, error) {
 	secs, err := strconv.ParseInt(str, 0, 64)
 	if err != nil {
 		return nil, err
@@ -61,10 +33,31 @@ func unixTimeUnmarshalStr(str string) (*UnixTime, error) {
 // UnmarshalText implements the [encoding.TextUnmarshaler] interface.
 // Time zone of the unmarshaled value is [time.UTC].
 func (t *UnixTime) UnmarshalText(text []byte) error {
-	ut, err := unixTimeUnmarshalStr(string(text))
+	ut, err := unmarshalStr(string(text))
 	if err != nil {
 		return err
 	}
 	*t = *ut
 	return nil
+}
+
+// used for testing
+func parseSecs(s uint64) UnixTime {
+	return UnixTime(time.Unix(int64(s), 0).In(time.UTC))
+}
+
+// ParseSecs parses seconds to UnixTime.
+func (t *UnixTime) ParseSecs(s uint64) {
+	*t = parseSecs(s)
+}
+
+// used for testing
+func parseMSecs(ms uint64) UnixTime {
+	sec, msec := bits.Div64(0, ms, 1000)
+	return UnixTime(time.Unix(int64(sec), int64(msec*1000000)).In(time.UTC))
+}
+
+// ParseMSecs parses milliseconds to UnixTime.
+func (t *UnixTime) ParseMSecs(ms uint64) {
+	*t = parseMSecs(ms)
 }
--- unixtime_test.go
+++ unixtime_test.go
@@ -1,87 +1,10 @@
 package timehelper
 
 import (
-	"encoding/binary"
 	"reflect"
 	"testing"
 	"time"
 )
-
-func TestUnixTime_MarshalBinary(t *testing.T) {
-	tests := []struct {
-		name string
-		ut   UnixTime
-		want int64
-	}{
-		{name: "zero time",
-			ut:   UnixTime(time.Time{}),
-			want: -62135596800,
-		},
-		{name: "1",
-			ut:   UnixTime(TimeYMD(2006, 1, 2)),
-			want: 1136160000,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotbb, _ := tt.ut.MarshalBinary()
-			got, _ := binary.Varint(gotbb)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UnixTime.MarshalBinary() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_unixTimeUnmarshalBin(t *testing.T) {
-	utz := UnixTime(time.Time{})
-	bbz, _ := utz.MarshalBinary()
-	ut0 := UnixTime(TimeYMD(1970, 1, 1))
-	ut1 := UnixTime(TimeYMD(2006, 1, 2))
-	bb1, _ := ut1.MarshalBinary()
-	type args struct {
-		bb []byte
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *UnixTime
-		wantErr bool
-	}{
-		{name: "00",
-			args:    args{},
-			wantErr: true,
-		},
-		{name: "01",
-			args:    args{bb: make([]byte, 0)},
-			wantErr: true,
-		},
-		{name: "zero time",
-			args: args{bb: bbz},
-			want: &utz,
-		},
-		{name: "0",
-			args: args{bb: []byte{0}},
-			want: &ut0,
-		},
-		{name: "1",
-			args: args{bb: bb1},
-			want: &ut1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := unixTimeUnmarshalBin(tt.args.bb)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("unixTimeUnmarshalBin() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("unixTimeUnmarshalBin() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestUnixTime_String(t *testing.T) {
 	tests := []struct {
@@ -103,7 +26,7 @@ func TestUnixTime_String(t *testing.T) {
 	}
 }
 
-func Test_unixTimeUnmarshalStr(t *testing.T) {
+func Test_unmarshalStr(t *testing.T) {
 	ut1 := UnixTime(TimeYMD(2006, 1, 2))
 	type args struct {
 		str string
@@ -129,13 +52,67 @@ func Test_unixTimeUnmarshalStr(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := unixTimeUnmarshalStr(tt.args.str)
+			got, err := unmarshalStr(tt.args.str)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("unixTimeUnmarshalStr() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("unmarshalStr() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("unixTimeUnmarshalStr() = %v, want %v", got, tt.want)
+				t.Errorf("unmarshalStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseSecs(t *testing.T) {
+	type args struct {
+		s uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want UnixTime
+	}{
+		{name: "0",
+			args: args{s: 0},
+			want: UnixTime(TimeYMD(1970, 1, 1)),
+		},
+		{name: "1",
+			args: args{s: 1257894000},
+			want: UnixTime(time.Date(2009, 11, 10, 23, 0, 0, 0, time.UTC)),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseSecs(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseSecs() = %v, want %v", time.Time(got), time.Time(tt.want))
+			}
+		})
+	}
+}
+
+func Test_parseMSecs(t *testing.T) {
+	type args struct {
+		ms uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want UnixTime
+	}{
+		{name: "0",
+			args: args{ms: 0},
+			want: UnixTime(TimeYMD(1970, 1, 1)),
+		},
+		{name: "1",
+			args: args{ms: 1702828942634},
+			want: UnixTime(time.Date(2023, 12, 17, 16, 2, 22, 634000000, time.UTC)),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseMSecs(tt.args.ms); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseMSecs() = %v, want %v", time.Time(got), time.Time(tt.want))
 			}
 		})
 	}
