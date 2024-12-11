d7772e2e822689a9b707eae42b6f1121b225ecb8
Oleg Subachev <git@solsw.ru>
2024-12-11 21:29:48 +0500

    BuildTime docs updated

--- buildtime.go
+++ buildtime.go
@@ -7,6 +7,11 @@ import (
 )
 
 // BuildTime parses 'buildDateTimeStr' as local time set by -ldflags during build process.
+//
+// First, declare 'var buildDateTimeStr string' in the 'main' package.
+// Then, add '-ldflags' build flag to 'go build' command.
+// Under Windows: -ldflags "-X 'main.buildDateTimeStr=%DATE%T%TIME%'".
+// Under Linux: -ldflags "-X 'main.buildDateTimeStr=$(date +%Y-%m-%dT%H:%M:%S)'".
 func BuildTime(buildDateTimeStr string) (time.Time, error) {
 	if cidx := strings.LastIndex(buildDateTimeStr, ","); cidx >= 0 {
 		buildDateTimeStr = buildDateTimeStr[:cidx]
